5d10cf438ff2a1c4b49d95dda07cb670d7d4907a
dhruv5604 <dhruvsolanki5604@gmail.com>
2025-04-14 17:21:31 +0530

    implemented measure convertor

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Measurement Converter</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container-fluid" class="d-flex flex-wrap text-center">
+        <h2>Measurement Conversion</h2>
+
+        <div id="length" class="m-3 p-3">
+            <a href="length">
+                <button>Length and Distance</button>
+            </a>
+        </div>
+
+        <div id="area" class="m-3 p-3">
+            <a href="area">
+                <button>Area</button>
+            </a>
+        </div>
+
+        <div id="volume" class="m-3 p-3">
+            <a href="volume">
+                <button>volume and capacity</button>
+            </a>
+        </div>
+
+        <div id="mass" class="m-3 p-3">
+            <a href="mass">
+                <button>Mass and Weight</button>
+            </a>
+        </div>
+
+        <div id="speed" class="m-3 p-3">
+            <a href="speed">
+                <button>Speed</button>
+            </a>
+        </div>
+
+        <div id="temperature" class="m-3 p-3">
+            <a href="temperature">
+                <button>Temperature</button>
+            </a>
+        </div>
+
+        <div id="currency" class="m-3 p-3">
+            <a href="currency">
+                <button>Currency</button>
+            </a>
+        </div>
+    </div>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ speed.php
@@ -0,0 +1,46 @@
+<?php
+require('type.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Measurement Converter</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <h2>Speed Converter</h2>
+    <form method="POST">
+        From:
+        <input type="hidden" id="type" name="type" value="speed">
+        <input type="number" name="value" id="input" required>
+        <select name="from_unit" id="opt1" name="opt1">
+            <?php
+            foreach ($kmph as $key => $value) {
+                echo "<option value='$key'>$key</option>";
+            }
+            ?>
+        </select>
+        <br>
+        To:
+        <input type="text" class="output" readonly id="output" />
+        <select name="to_unit" id="opt2" name="opt2">
+            <?php
+            foreach ($kmph as $key => $value) {
+                echo "<option value='$key'>$key</option>";
+            }
+            ?>
+        </select>
+    </form>
+    <br><br>
+    <a href="/">Return to menu</a>
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
